76ffd31f035be4c237925ea97a78c9369f60782d
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2026-03-29 12:33:23 -0500

    add express dependency

--- packages/server/config/express.ts
+++ packages/server/config/express.ts
@@ -1,6 +1,6 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from "cors"
+import 'dotenv/config';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());

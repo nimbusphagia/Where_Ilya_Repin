67aafcd3b67c39d01898512a5637a67b8663a1a1
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2026-04-02 15:45:56 -0500

    change main subMenu for Navlinks

--- packages/client/src/pages/Menu/Menu.tsx
+++ packages/client/src/pages/Menu/Menu.tsx
@@ -1,26 +1,15 @@
 import s from './Menu.module.css'
 import gs from "../../main.module.css"
-import { useLoaderData } from 'react-router'
+import { NavLink, useLoaderData } from 'react-router'
 import type { Menu } from './types/ui';
 import { LevelItem } from './components/LevelItem';
-import { SubMenu } from '../../components/SubMenu';
 import { useState } from 'react';
 import type { LevelInput } from '../../schemas/level.schema';
 
 export function Menu() {
   const levels = useLoaderData<LevelInput[]>();
   const [selectedLvl, setSelectedLvl] = useState<LevelInput>(levels[0]);
-  const menuActions = [
-    {
-      name: "Play",
-      path: `/game/${selectedLvl.id}`,
-      intent: "play",
-    },
-    {
-      name: "Leaderboard",
-      path: "",
-      intent: "showLeaderboard"
-    }];
+
   return (
     <main
       className={s.body}
@@ -51,11 +40,18 @@ export function Menu() {
           })
         }
       </div>
-      <div className={s.subMenuContainer}>
-        <SubMenu
-          actions={menuActions}
-          className={s.subMenu}
-        />
+      <div className={s.navContainer}>
+        <div className={s.nav}>
+          <NavLink
+            to={`/game/${selectedLvl.id}`}
+            className={s.navLink}
+          >Play</NavLink>
+          <NavLink
+            to={`/leaderboard`}
+            className={s.navLink}
+
+          >Leaderboard</NavLink>
+        </div>
 
       </div>
     </main>
